1f3675893d18e077b1625784c26733a261856c4c
baichou <lvluo200711@gmail.com>
2022-06-02 17:07:05 +0800

    swagger

--- console/commands/z_inject_gen.go
+++ console/commands/z_inject_gen.go
@@ -2,11 +2,12 @@
 package commands
 
 import (
-	app "github.com/go-home-admin/home/bootstrap/services/app"
+	providers "github.com/go-home-admin/home/bootstrap/providers"
 )
 
 var _BeanCommandSingle *BeanCommand
 var _DevCommandSingle *DevCommand
+var _JsSingle *Js
 var _OrmCommandSingle *OrmCommand
 var _ProtocCommandSingle *ProtocCommand
 var _RouteCommandSingle *RouteCommand
@@ -16,6 +17,7 @@ func GetAllProvider() []interface{} {
 	return []interface{}{
 		NewBeanCommand(),
 		NewDevCommand(),
+		NewJs(),
 		NewOrmCommand(),
 		NewProtocCommand(),
 		NewRouteCommand(),
@@ -26,42 +28,49 @@ func GetAllProvider() []interface{} {
 func NewBeanCommand() *BeanCommand {
 	if _BeanCommandSingle == nil {
 		_BeanCommandSingle = &BeanCommand{}
-		app.AfterProvider(_BeanCommandSingle, "")
+		providers.AfterProvider(_BeanCommandSingle, "")
 	}
 	return _BeanCommandSingle
 }
 func NewDevCommand() *DevCommand {
 	if _DevCommandSingle == nil {
 		_DevCommandSingle = &DevCommand{}
-		app.AfterProvider(_DevCommandSingle, "")
+		providers.AfterProvider(_DevCommandSingle, "")
 	}
 	return _DevCommandSingle
+}
+func NewJs() *Js {
+	if _JsSingle == nil {
+		_JsSingle = &Js{}
+		providers.AfterProvider(_JsSingle, "")
+	}
+	return _JsSingle
 }
 func NewOrmCommand() *OrmCommand {
 	if _OrmCommandSingle == nil {
 		_OrmCommandSingle = &OrmCommand{}
-		app.AfterProvider(_OrmCommandSingle, "")
+		providers.AfterProvider(_OrmCommandSingle, "")
 	}
 	return _OrmCommandSingle
 }
 func NewProtocCommand() *ProtocCommand {
 	if _ProtocCommandSingle == nil {
 		_ProtocCommandSingle = &ProtocCommand{}
-		app.AfterProvider(_ProtocCommandSingle, "")
+		providers.AfterProvider(_ProtocCommandSingle, "")
 	}
 	return _ProtocCommandSingle
 }
 func NewRouteCommand() *RouteCommand {
 	if _RouteCommandSingle == nil {
 		_RouteCommandSingle = &RouteCommand{}
-		app.AfterProvider(_RouteCommandSingle, "")
+		providers.AfterProvider(_RouteCommandSingle, "")
 	}
 	return _RouteCommandSingle
 }
 func NewSwaggerCommand() *SwaggerCommand {
 	if _SwaggerCommandSingle == nil {
 		_SwaggerCommandSingle = &SwaggerCommand{}
-		app.AfterProvider(_SwaggerCommandSingle, "")
+		providers.AfterProvider(_SwaggerCommandSingle, "")
 	}
 	return _SwaggerCommandSingle
 }
